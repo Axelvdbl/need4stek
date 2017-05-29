5f342e278ab7b92e568936f15c47c5095a8ae3de
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2017-05-29 14:52:20 +0200

    new_func

--- include/command.h
+++ include/command.h
@@ -5,7 +5,7 @@
 ** Login   <alexandre.chamard-bois@epitech.eu@epitech.eu>
 **
 ** Started on  Mon May 29 13:13:34 2017 Alexandre Chamard-bois
-** Last update Mon May 29 14:50:15 2017 Alexandre Chamard-bois
+** Last update Mon May 29 14:52:17 2017 Alexandre Chamard-bois
 */
 
 #ifndef COMMAND_H_
@@ -32,14 +32,14 @@ typedef union   u_out
   long          stime[2];
 }               t_out;
 
-int start(t_out *pars_out);
-int stop(t_out *pars_out);
-int forward(t_out *pars_out, float nb);
-int backwards(t_out *pars_out, float nb);
-int wheels_dir(t_out *pars_out, float nb);
+int start();
+int stop();
+int forward(float nb);
+int backwards(float nb);
+int wheels_dir(float nb);
 int lidar(t_out *pars_out);
 int get_wheels(t_out *pars_out);
-int cylce_wait(t_out *pars_out, int nb);
+int cylce_wait(int nb);
 int get_speed_max(t_out *pars_out);
 int get_speed_min(t_out *pars_out);
 int get_simtime(t_out *pars_out);
